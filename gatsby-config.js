12b6a1ceefcc4f0129fcd85208fd7612ddab3867
benballance <ben.ballance@gmail.com>
2020-05-03 09:58:35 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
+    title: `BenBallance.com`,
     author: `Ben Ballance`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://www.benballance.com/`,
     social: {
       twitter: `kylemathews`,
     },
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -15,7 +15,7 @@ class Blog extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
+        <SEO title="All Blog Posts | BenBallance.com" />
         <Bio />
         <div style={{ margin: "20px 0 40px" }}>
           {posts.map(({ node }) => {
